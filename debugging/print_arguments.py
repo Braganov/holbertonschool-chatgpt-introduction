31dcffc40a9bc8afb4d1349f91a91b154f8010a9
Braga <ibrahim.houmaidi@gmail.com>
2025-01-13 10:15:49 +0100

    update code Debugging - Python Arguments

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-# Iterate over the command-line arguments and print them
-for i, arg in enumerate(sys.argv):
-    print(f"Argument {i}: {arg}")
+for i in range(len(sys.argv)):
+    print(sys.argv[i])
